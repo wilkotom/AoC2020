1f941c4ef84a88e17b11db5464f7b28071a28659
Tom Wilkinson <tom+github@dentrasssi.net>
2020-12-08 19:18:06 +0000

    Readability changes

--- 6/main.py
+++ 6/main.py
@@ -2,8 +2,8 @@ part1 = 0
 part2 = 0
 for group in open("input.txt").read().split('\n\n'):
     answers = [set(x) for x in group.split('\n')]
-    part1 += len(answers[0].union(*answers[1:]))
-    part2 += len(answers[0].intersection(*answers[1:]))
+    part1 += len(set.union(*answers))
+    part2 += len(set.intersection(*answers))
 
 print(f"Part 1 answer: {part1}")
 print(f"Part 2 answer: {part2}")

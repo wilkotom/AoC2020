41d2547318f7d621deecdabd8dca2e1f98f5fec6
Tom Wilkinson <tom+github@dentrasssi.net>
2020-12-09 08:32:30 +0000

    Day 9. Somewhat brute force-y.

---
+++ 9/main.py
@@ -0,0 +1,28 @@
+
+def get_pairs(nums: list[int]) -> list[int]:
+    return [nums[0] + n for n in nums[1:]] + get_pairs(nums[1:]) if nums else []
+
+
+def get_contiguous(nums: list[int], target: int) -> list[int]:
+    if not nums:
+        return []
+    for i in range(len(nums)):
+        if sum(nums[0:i]) == target:
+            return nums[0:i]
+    return get_contiguous(nums[1:], target)
+
+
+def main(filename: str, preamble_size: int) -> None:
+    numbers = [int(l) for l in open(filename)]
+    solution = 0
+    for i, val in enumerate(numbers[preamble_size:]):
+        if val not in get_pairs(numbers[i:i+preamble_size]):
+            solution = val
+            break
+    print(f"Part 1: {solution}")
+    contiguous = get_contiguous(numbers, solution)
+    print(f"Part 2: {min(contiguous) + max(contiguous)}")
+
+
+if __name__ == "__main__":
+    main("input.txt", 25)

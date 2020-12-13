6aa3b4234d637d373c7b5b45083c1781eeb4ec24
Tom Wilkinson <tom+github@dentrasssi.net>
2020-12-13 11:47:36 +0000

    Day 12: Turns out Python already calculates inverse mod for you. Who knew? (not me)

--- 13/main.py
+++ 13/main.py
@@ -4,18 +4,8 @@ from functools import reduce
 def chinese_remainder_gaussian(a, mod_prod, n):
     result = 0
     for i in range(len(n)):
-        result += a[i] * mod_prod // n[i] * inverse_mod((mod_prod // n[i]), n[i])
+        result += a[i] * mod_prod // n[i] * pow((mod_prod // n[i]), -1, n[i])
     return result % mod_prod
-
-
-def inverse_mod(a: int, b: int) -> tuple[int, int, int]:
-    d = b
-    x0, x1, y0, y1 = 0, 1, 1, 0
-    while a != 0:
-        (q, a), b = divmod(b, a), a
-        y0, y1 = y1, y0 - q * y1
-        x0, x1 = x1, x0 - q * x1
-    return x0 % d
 
 
 def main(filename: str) -> None:

c56e8091c888f57fd1141b178235757a4381b4c2
Tom Wilkinson <tom+github@dentrasssi.net>
2020-12-17 10:39:04 +0000

    Day 17: Conway's n-dimensional game of life

---
+++ 17/main.py
@@ -0,0 +1,66 @@
+from itertools import product
+from typing import Iterator
+
+
+def read_grid(filename: str, dimensions: int) -> set[tuple]:
+    starting_state = set()
+    grid = open(filename).read().split('\n')
+    for y in range(len(grid)):
+        for x in range(len(grid)):
+            if grid[y][x] == '#':
+                starting_state.add(tuple([x, y] + [0] * (dimensions -2)))
+    return starting_state
+
+
+def get_bounds(grid: set[tuple[int, ...], bool]) -> list[tuple[int, ...]]:
+    result = []
+    dimension_values = list(zip(*grid))
+    for i in range(len(dimension_values)):
+        result.append((min(dimension_values[i]), max(dimension_values[i])))
+    return result
+
+
+def get_neighbours(dimensions: int) -> list[tuple[int, ...]]:
+    neighbours = list(product(*([[-1, 0, 1]] * dimensions)))
+    home = tuple([0] * dimensions)
+    neighbours.remove(home)
+    return neighbours
+
+
+def get_cubes(boundaries: list[tuple[int, ...]]) -> Iterator[tuple[int, ...]]:
+    return product(*[range(x[0]-1, x[1]+2) for x in boundaries])
+
+
+def cube_neighbours(cube: tuple[int, ...], neighbours: list[tuple[int, ...]]) -> list[tuple[int, ...]]:
+    result = []
+    for neighbour in neighbours:
+        result.append(tuple(sum(x) for x in zip(cube, neighbour)))
+    return result
+
+
+def n_dimensional_conway(filename: str, dimensions: int, generations: int) -> int:
+    grid = read_grid(filename, dimensions)
+    neighbours = get_neighbours(dimensions)
+    new_grid = set()
+    for i in range(generations):
+        boundaries = get_bounds(grid)
+        possible_cubes = get_cubes(boundaries)
+        for cube in possible_cubes:
+            active_neighbours = 0
+            for neighbour in cube_neighbours(cube,neighbours):
+                if neighbour in grid:
+                    active_neighbours += 1
+            if active_neighbours == 3 or (active_neighbours == 2 and cube in grid):
+                new_grid.add(cube)
+        grid = new_grid
+        new_grid = set()
+    return len(grid)
+
+
+def main(filename: str) -> None:
+    print(f"Part 1 Answer: {n_dimensional_conway(filename=filename, dimensions=3, generations=6)}")
+    print(f"Part 2 Answer: {n_dimensional_conway(filename=filename, dimensions=4, generations=6)}")
+
+
+if __name__ == "__main__":
+    main("input.txt")

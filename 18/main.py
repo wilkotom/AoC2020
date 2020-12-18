157698fed7c4fc7b31d6e4a2c21913df299dc4a4
Tom Wilkinson <tom+github@dentrasssi.net>
2020-12-18 13:29:16 +0000

    Day 18: backwards-precedence mathematics!

--- 18/main.py
+++ 18/main.py
@@ -2,12 +2,12 @@ from functools import reduce
 from typing import Union, Callable
 
 
-def collapse_part_1(expression: list[Union[int, str]]) -> int:
+def eval_part_1(expression: list[Union[int, str]]) -> int:
     expression[2] = expression[0] + expression[2] if expression[1] == "+" else expression[0] * expression[2]
-    return evaluate_expression(expression[2:], collapse_part_1)
+    return evaluate_expression(expression[2:], eval_part_1)
 
 
-def collapse_part_2(expression: list[Union[int, str]]) -> int:
+def eval_part_2(expression: list[Union[int, str]]) -> int:
 
     def collapse(expr: list[Union[int, str]]) -> list[Union[int, str]]:
         if len(expr) == 1:
@@ -20,11 +20,11 @@ def collapse_part_2(expression: list[Union[int, str]]) -> int:
     return reduce(lambda x, y: x * y, collapse(expression)[::2])
 
 
-def evaluate_expression(expression: list[Union[int, str]], collapse: Callable[[list[Union[int,str]]], int]) -> int:
+def evaluate_expression(expression: list[Union[int, str]], evaluate: Callable[[list[Union[int, str]]], int]) -> int:
     if len(expression) == 1:
         return expression[0]
     if '(' not in expression:
-        return collapse(expression)
+        return evaluate(expression)
     else:
         start = expression.index('(')
         counter = i = 0
@@ -37,15 +37,15 @@ def evaluate_expression(expression: list[Union[int, str]], collapse: Callable[[l
                 break
         new_expression = expression[start+1:i]
         return evaluate_expression(
-            expression[:start] + [evaluate_expression(new_expression, collapse)] + expression[i + 1:], collapse)
+            expression[:start] + [evaluate_expression(new_expression, evaluate)] + expression[i + 1:], evaluate)
 
 
 def main(filename: str) -> None:
     total_part_1 = 0
     total_part_2 = 0
     for expression in (l.strip().replace(' ', '') for l in open(filename).readlines()):
-        total_part_1 += evaluate_expression([x if x in "+*()" else int(x) for x in expression], collapse_part_1)
-        total_part_2 += evaluate_expression([x if x in "+*()" else int(x) for x in expression], collapse_part_2)
+        total_part_1 += evaluate_expression([x if x in "+*()" else int(x) for x in expression], eval_part_1)
+        total_part_2 += evaluate_expression([x if x in "+*()" else int(x) for x in expression], eval_part_2)
     print(total_part_1, total_part_2)
 
 
